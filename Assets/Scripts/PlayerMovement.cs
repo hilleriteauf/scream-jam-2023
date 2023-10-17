5f354872ef5731654a8b9bd852c043aac30dee31
Frédéric Hilleriteau <46034382+hilleriteauf@users.noreply.github.com>
2023-10-17 20:43:38 +0200

    Dash implementation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,7 +7,13 @@ public class PlayerMovement : MonoBehaviour
 
     public CharacterController controller;
 
-    public float speed = 12f;
+    public float walkingSpeed = 12f;
+
+    public float dashDuration = 0.25f;
+    public float dashSpeed = 32f;
+
+
+    private float lastDash = 0f;
 
     // Start is called before the first frame update
     void Start()
@@ -18,8 +24,22 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float x = Input.GetAxis("Horizontal"); // A and D keys
-        float z = Input.GetAxis("Vertical"); // W and S keys
+        float x = Input.GetAxis("Horizontal"); // Q and D keys
+        float z = Input.GetAxis("Vertical"); // Z and S keys
+
+        float speed = walkingSpeed;
+
+        // Dash trigger
+        if (Input.GetKeyDown(KeyCode.LeftShift) && (Time.time - lastDash > dashDuration))
+        {
+            lastDash = Time.time;
+        }
+
+        // Apply dash speed
+        if (Time.time - lastDash < dashDuration)
+        {
+            speed = dashSpeed;
+        }
 
         Vector3 move = transform.right * x + transform.forward * z; // Move in the direction of the camera
 
